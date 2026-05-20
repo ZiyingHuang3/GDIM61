9e667722b8407160eac478e5b70b92751f160652
ZiyingHuang3 <ziyinh3@uci.edu>
2026-05-19 19:41:14 -0700

    1

--- Assets/Script/Part2EvidenceChecker.cs
+++ Assets/Script/Part2EvidenceChecker.cs
@@ -21,7 +21,7 @@ public class Part2EvidenceChecker : MonoBehaviour
             Debug.Log("Part 2 evidence complete!");
           DialogueManager.Instance.StartSingleLineDialogue(
     "Cat",
-    "You've completed all the investigations and interrogations. Please return to where everything began.Hint: Evidence can no longer be re-examined."
+    "The case is reaching its end, detective. When you're ready, follow the arrow and make your final deduction."
 );  
         }
     }
